037db16175c57a107433961453a91a031294ba05
hs-balashov <balashov@hispot.co.jp>
2023-03-20 15:07:45 +0900

    fix ruby-lint

--- shell/prepare-lint-variables.sh
+++ shell/prepare-lint-variables.sh
@@ -7,5 +7,5 @@ xml_loc=${loc}/lint-results-${variant}.xml
 html_loc=${loc}/lint-results-${variant}.html
 
 envman add --key LINT_XML_OUTPUT --value $xml_loc
-envman add --key LINT_XML_OUTPUT_RUBY --value ../../${xml_loc}
+envman add --key LINT_XML_OUTPUT_RUBY --value ../../build/reports/lint-results-${variant}.xml
 envman add --key LINT_HTML_OUTPUT --value $html_loc
