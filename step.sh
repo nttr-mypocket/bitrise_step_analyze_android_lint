b3238ca9e96a0df6dea9b9c078ee4b9556c79fbb
hs-balashov <balashov@hispot.co.jp>
2023-03-20 09:27:30 +0900

    print files and folders in current folder (test)

--- step.sh
+++ step.sh
@@ -3,6 +3,9 @@ set -ex
 
 # Print the current folder
 echo "Current folder: $(pwd)"
+# Print the files and folders in the current directory
+echo "Files and folders in the current directory:"
+ls
 
 chmod +x ./shell/prepare-lint-variables.sh
 source ./shell/prepare-lint-variables.sh
