017b5ac47d90a80445c121b97ab8da1cd03a4cb7
hs-balashov <balashov@hispot.co.jp>
2023-03-20 16:09:06 +0900

    update env vars definition

--- shell/prepare-lint-variables.sh
+++ shell/prepare-lint-variables.sh
@@ -11,10 +11,8 @@ loc=${loc}/${lint_module}/build/reports
 xml_loc=${loc}/lint-results-${variant}.xml
 html_loc=${loc}/lint-results-${variant}.html
 
-envman add --key LINT_XML_OUTPUT --value $xml_loc
-envman add --key LINT_HTML_OUTPUT --value $html_loc
-
-env
+export LINT_XML_OUTPUT=$xml_loc
+export LINT_HTML_OUTPUT=$html_loc
 
 echo $LINT_XML_OUTPUT
 echo $LINT_HTML_OUTPUT
