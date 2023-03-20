6fc91a97ab9ec208b7911016001de5c0e78ce1af
hs-balashov <balashov@hispot.co.jp>
2023-03-20 17:41:08 +0900

    fix env vars

--- shell/prepare-lint-variables.sh
+++ shell/prepare-lint-variables.sh
@@ -13,5 +13,8 @@ file_loc=${loc}/lint-results-${variant}
 export LINT_XML_OUTPUT=${file_loc}.xml
 export LINT_HTML_OUTPUT=${file_loc}.html
 
+envman add --key LINT_XML_OUTPUT --value $xml_loc
+envman add --key LINT_HTML_OUTPUT --value $html_loc
+
 echo $LINT_XML_OUTPUT
 echo $LINT_HTML_OUTPUT
