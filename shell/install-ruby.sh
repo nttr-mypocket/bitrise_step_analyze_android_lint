4559821f61059a534644ca0de939e5dd30c99e69
hs-balashov <balashov@hispot.co.jp>
2023-03-20 14:48:32 +0900

    update ruby installation

--- shell/install-ruby.sh
+++ shell/install-ruby.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 set -ex
 
-# Install RVM if it's not already installed
-if [[ ! -s "$HOME/.rvm/scripts/rvm" ]]; then
-  \curl -sSL https://get.rvm.io | bash -s stable
-fi
-
-ruby_version=2.7.5
-
-source "$HOME/.rvm/scripts/rvm"
-
-rvm install $ruby_version
-rvm use $ruby_version
+# Install Ruby
+apt-get update
+apt-get install -y ruby
 
 ruby --version
--- step.sh
+++ step.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ex
 
-git clone https://github.com/nttr-mypocket/bitrise_step_analyze_android_lint.git test-folder
+folder="step-folder"
+
+git clone https://github.com/nttr-mypocket/bitrise_step_analyze_android_lint.git $folder
 
 # Print the current folder
 echo "Current folder: $(pwd)"
@@ -9,10 +11,10 @@ echo "Current folder: $(pwd)"
 echo "Files and folders in the current directory:"
 ls
 
-chmod +x ./test-folder/shell/prepare-lint-variables.sh
-source ./test-folder/shell/prepare-lint-variables.sh
+chmod +x ./${$folder}/shell/prepare-lint-variables.sh
+source ./${$folder}/shell/prepare-lint-variables.sh
 
-chmod +x ./test-folder/shell/install-ruby.sh
-source ./test-folder/shell/install-ruby.sh
+chmod +x ./${$folder}/shell/install-ruby.sh
+source ./${$folder}/shell/install-ruby.sh
 
-ruby ./test-folder/ruby/android-lint.rb
+ruby ./${$folder}/ruby/android-lint.rb
