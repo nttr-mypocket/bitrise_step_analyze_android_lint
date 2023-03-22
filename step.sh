a010da47ac5249b619d7f16dc2d2adccfe067e6a
hs-kinkori <kinkori@hispot.co.jp>
2023-03-22 15:18:40 +0900

    コメントを充実化

--- step.sh
+++ step.sh
@@ -3,24 +3,42 @@ set -ex
 set +x
 set +v
 
-folder=$step_clone_dir_branch
+echo "Start Lint Analyze"
 
-# ステップのリポジトリをクローンする
-git clone -b $step_branch $step_repository_url $folder
+echo "Print Enviroments"
 
-loc=$target_project_location
+echo "  target_project_location: $target_project_location"
+echo "  target_module: $target_module"
+echo "  build_variant: $build_variant"
+echo "  step_repository_url: $step_repository_url"
+echo "  step_clone_dir_branch: $step_clone_dir_branch"
+echo "  step_branch: $step_branch"
+
+echo "Generate Enviroments"
+scripts_dir=$step_clone_dir_branch
 lint_module=$target_module
 variant=$build_variant
+file_loc=${target_project_location}/${lint_module}/build/reports/lint-results-${variant}
 
-# lintの実行結果を保存するファイルのパスを設定する
-loc=${loc}/${lint_module}/build/reports
-file_loc=${loc}/lint-results-${variant}
+echo "  scripts_dir: $scripts_dir"
+echo "  lint_module: $lint_module"
+echo "  variant: $variant"
+echo "  file_loc: $file_loc"
 
-# 環境変数を設定する
+# export
 export LINT_XML_OUTPUT=${file_loc}.xml
 export LINT_HTML_OUTPUT=${file_loc}.html
-envman add --key LINT_XML_OUTPUT --value ${file_loc}.xml
-envman add --key LINT_HTML_OUTPUT --value ${file_loc}.html
+
+# ステップのリポジトリをクローンする
+echo "Prepare Scripts file, with Git Clone. Dir: $scripts_dir"
+git clone -b $step_branch $step_repository_url $scripts_dir
+echo "Prepared Scripts file."
+
+# 環境変数を設定する
+envman add --key LINT_XML_OUTPUT --value ${LINT_XML_OUTPUT}
+envman add --key LINT_HTML_OUTPUT --value ${LINT_HTML_OUTPUT}
 
 # rubyを実行する
 ruby ./${folder}/ruby/android-lint.rb
+
+echo "Complete Lint Analyze"
