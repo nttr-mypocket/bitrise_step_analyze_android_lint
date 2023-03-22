bc6e9df5d8f59369c768cc274579196b46767bf2
hs-balashov <balashov@hispot.co.jp>
2023-03-22 13:35:49 +0900

    refactor with adding comments

--- ruby/android-lint.rb
+++ ruby/android-lint.rb
@@ -3,8 +3,9 @@
 require 'rexml/document'
 require 'logger'
 
+# Issue構造体の定義
 Issue = Struct.new(
-  :id,
+  :id, 
   :severity,
   :message,
   :category,
@@ -16,12 +17,14 @@ Issue = Struct.new(
   :location
 )
 
+# Location構造体の定義
 Location = Struct.new(
   :file,
   :line,
   :column
 )
 
+# エラーのカウントを出力する関数
 def print_error_counts(logger, error_count, fatal_count, ignore_count, informational_count, warning_count)
   logger.info("error count: #{error_count}")
   logger.info("fatal count: #{fatal_count}")
@@ -30,6 +33,7 @@ def print_error_counts(logger, error_count, fatal_count, ignore_count, informati
   logger.info("warning count: #{warning_count}")
 end
 
+# Locationオブジェクトを生成する関数
 def build_location(xml_element)
   location_element = xml_element.elements['location']
   Location.new(
@@ -39,20 +43,25 @@ def build_location(xml_element)
   )
 end
 
+# ロガーを設定する
 logger = Logger.new($stderr)
 
+# Lintの出力ファイルを取得する
 xml_file = ENV["LINT_XML_OUTPUT"]
-logger.info("INPUT XML: #{xml_file}")
+logger.info("入力XML: #{xml_file}")
 
+# エラーの種類ごとにカウントするための変数を初期化する
 error_count  = 0
 fatal_count  = 0
 ignore_count = 0
 informational_count = 0
 warning_count = 0
 
+# Lintの出力ファイルをパースして問題を取得する
 begin
   doc = REXML::Document.new(File.open(xml_file))
   doc.elements.each('//issues/issue') do |issue_element|
+    # XML要素からIssueオブジェクトを作成する
     issue = Issue.new(
       issue_element['id'],
       issue_element['severity'],
@@ -66,6 +75,7 @@ begin
       build_location(issue_element)
     )
 
+    # エラーの種類ごとにカウントする
     if 'error'.casecmp(issue.severity).zero?
       error_count  += 1
     elsif 'fatal'.casecmp(issue.severity).zero?
@@ -80,20 +90,24 @@ begin
 
   end
 rescue StandardError => e
-  logger.error("Failed to open XML file: #{xml_file}")
-  logger.error("Error: #{e.message}")
+  # ファイルのオープンに失敗した場合はエラーをログに出力して終了する
+  logger.error("XMLファイルを開くのに失敗しました: #{xml_file}")
+  logger.error("エラー: #{e.message}")
   exit(1)
 end
 
+# エラーの種類ごとにカウントした結果をログに出力する
 print_error_counts(logger, error_count, fatal_count, ignore_count, informational_count, warning_count)
 
+# エラーの種類ごとにカウントした結果をログに出力する
 system( "envman add --key LINT_OUTPUT_ERROR --value #{error_count}" )
 system( "envman add --key LINT_OUTPUT_FATAL --value #{fatal_count}" )
 system( "envman add --key LINT_OUTPUT_IGNORE --value #{ignore_count}" )
 system( "envman add --key LINT_OUTPUT_INFO --value #{informational_count}" )
 system( "envman add --key LINT_OUTPUT_WARNING --value #{warning_count}" )
 
+# エラーカウントがある場合、エラーログを出力し、スクリプトを終了する
 if error_count.positive? || fatal_count.positive?
-  logger.error("Critical errors count : #{error_count + fatal_count}")
+  logger.error("致命的なエラーの数： #{error_count + fatal_count}")
   exit(2)
 end
--- step.sh
+++ step.sh
@@ -5,12 +5,22 @@ set +v
 
 folder="step-folder"
 
+# ステップのリポジトリをクロンする
 git clone https://github.com/nttr-mypocket/bitrise_step_analyze_android_lint.git $folder
 
-chmod +x ./${folder}/shell/prepare-lint-variables.sh
-source ./${folder}/shell/prepare-lint-variables.sh
+loc=$PROJECT_LOCATION
+lint_module=$MODULE
+variant=$VARIANT
 
-#chmod +x ./${folder}/shell/install-ruby.sh
-#source ./${folder}/shell/install-ruby.sh
+# lintの実行結果を保存するファイルのパスを設定する
+loc=${loc}/${lint_module}/build/reports
+file_loc=${loc}/lint-results-${variant}
 
+# 環境変数を設定する
+export LINT_XML_OUTPUT=${file_loc}.xml
+export LINT_HTML_OUTPUT=${file_loc}.html
+envman add --key LINT_XML_OUTPUT --value ${file_loc}.xml
+envman add --key LINT_HTML_OUTPUT --value ${file_loc}.html
+
+# rubyを実行する
 ruby ./${folder}/ruby/android-lint.rb
